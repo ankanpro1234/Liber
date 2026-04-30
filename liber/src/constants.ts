39cdbbe4b6085984c9ee4b4d9939f0df8add5fe0
ankanpro1234 <ankankarmakar2072006@gmail.com>
2026-04-30 23:17:23 +0530

    Add files via upload

---
+++ liber/src/constants.ts
@@ -0,0 +1,25 @@
+import { Book } from "./types";
+import { EXTENDED_BOOKS } from "./data/books";
+
+export const SAMPLE_BOOKS: Book[] = EXTENDED_BOOKS;
+
+export const CATEGORIES = [
+  "Contemporary Fiction",
+  "Science Fiction",
+  "Historical Fiction",
+  "Self-Help",
+  "Mythology",
+  "Literary Fiction",
+  "Memoir",
+  "Thriller",
+  "Fantasy",
+  "Mystery",
+  "Non-Fiction",
+  "Classics",
+  "Philosophy",
+  "Business",
+  "Biographies",
+  "Dystopian",
+  "Post-Apocalyptic"
+];
+
